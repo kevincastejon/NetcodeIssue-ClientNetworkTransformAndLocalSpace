3e4af1fe443d8245930e6c88a110f46a4562a2d5
Kevin Castejon <castejonkevin@gmail.com>
2025-03-01 20:16:03 +0100

    simplify

--- Assets/PlayerSpawner.cs
+++ Assets/PlayerSpawner.cs
@@ -1,16 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class PlayerSpawner : NetworkBehaviour
 {
-    private static PlayerSpawner _instance;
-
-    public static PlayerSpawner Instance { get => _instance; }
-
+    [SerializeField] private NetworkObject _playerPrefab;
+    private PlayableDirector _timeline;
     private void Awake()
     {
-        _instance = this;
+        _timeline = GetComponent<PlayableDirector>();
+    }
+    public override void OnNetworkSpawn()
+    {
+        if (IsServer)
+        {
+            _timeline.Play();
+        }
+        if (IsClient)
+        {
+            RequestPlayerObjectSpawnRpc();
+        }
+    }
+    [Rpc(SendTo.Server)]
+    private void RequestPlayerObjectSpawnRpc(RpcParams rpcParams = default)
+    {
+        NetworkObject playerObject = Instantiate(_playerPrefab);
+        playerObject.SpawnAsPlayerObject(rpcParams.Receive.SenderClientId);
+        playerObject.TrySetParent(NetworkObject, false);
+        SynchronizeTimelineRpc(_timeline.time, new RpcParams() { Send = new() { Target = RpcTarget.Single(rpcParams.Receive.SenderClientId, RpcTargetUse.Temp) } });
+    }
+    [Rpc(SendTo.SpecifiedInParams)]
+    private void SynchronizeTimelineRpc(double time, RpcParams rpcParams)
+    {
+        _timeline.time = time;
+        _timeline.Play();
     }
 }
